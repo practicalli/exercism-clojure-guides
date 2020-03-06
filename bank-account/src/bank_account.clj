d66277815ba64730649a964edefbd5978163737c
jr0cket <john@jr0cket.co.uk>
2020-03-06 16:29:30 +0000

    Bank Account - parrallelism with a Clojure atom
    Simulate a bank account supporting opening/closing, withdrawals, and deposits of
    money, which supports concurrent transactions.
    
    As functions will be called concurrently, a Clojure atom is used to manage any
    state changes of the bank accounts.  This ensures that the bank account can only
    be changed by one call at any one time.
    
    The bank account was defined as a hash-map, with user-id as a UUID (universal
    id) from the java.util library.

---
+++ bank-account/src/bank_account.clj
@@ -0,0 +1,43 @@
+(ns bank-account)
+
+
+(def bank-accounts
+  "All accounts for the bank
+  Each account has a unique id and a numeric value"
+  (atom {}))
+
+(defn open-account
+  "Open an account with a zero balance."
+  []
+  (let [account-id (java.util.UUID/randomUUID)]
+    (swap! bank-accounts assoc account-id 0)
+    account-id))
+
+(defn close-account [account-id]
+  (swap! bank-accounts dissoc account-id)
+  )
+
+(defn get-balance [account-id]
+  (get @bank-accounts account-id)
+  )
+
+#_(deref bank-accounts)
+
+(defn update-balance [account-id credit]
+  (swap! bank-accounts update account-id #(+ % credit)))
+
+#_(open-account)
+
+#_(= 0 (-> (open-account)
+           (get-balance)))
+
+
+#_(deref bank-accounts)
+;; => {#uuid "af0747fc-86bf-496a-8cc1-efb917887213" 0}
+
+#_(update-balance #uuid "af0747fc-86bf-496a-8cc1-efb917887213" 10)
+;; => {#uuid "af0747fc-86bf-496a-8cc1-efb917887213" 10}
+
+
+#_(update-balance #uuid "af0747fc-86bf-496a-8cc1-efb917887213" -10)
+;; => {#uuid "af0747fc-86bf-496a-8cc1-efb917887213" 0}
