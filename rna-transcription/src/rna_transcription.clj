6424f63a81218dbb641423a4041f929adfd8a36a
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-10-24 12:29:10 +0100

    RNATranscription: minor improvements
    Require clojure.string :as string
    
    Pretty format of the transcription mapping
    
    Add rich comment block to show how the main function operates

--- rna-transcription/src/rna_transcription.clj
+++ rna-transcription/src/rna_transcription.clj
@@ -1,17 +1,29 @@
 (ns rna-transcription
-  (:require clojure.string))
-
+  (:require [clojure.string :as string]))
 
 (def dna-nucleotide->rna-nucleotide
   "DNA to RNA transcription mappings"
-  {\G \C \C \G \T \A \A \U})
+  {\G \C
+   \C \G
+   \T \A
+   \A \U})
+
 
 (defn to-rna
   "Transcribe each nucleotide from a DNA strand into its RNA complement
   Arguments: string representing DNA strand
   Return: string representing RNA strand"
   [dna]
-  (clojure.string/join
+  (string/join
     (map #(or (dna-nucleotide->rna-nucleotide %)
               (throw (AssertionError. "Unknown nucleotide")))
          dna )))
+
+(comment
+  (to-rna "GATTAGA")
+  ;; => "CUAAUCU"
+
+  (to-rna "GX")
+
+
+  )
