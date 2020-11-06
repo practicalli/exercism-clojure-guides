b0eb96e5c42adeb9fa31d14723e94a8af40ff227
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-11-06 18:39:22 +0000

    Reverse String: alternative solutions

--- reverse-string/src/reverse_string.clj
+++ reverse-string/src/reverse_string.clj
@@ -1,5 +1,22 @@
 (ns reverse-string
   (:require clojure.string))
 
+
 (defn reverse-string [s]
-  (clojure.string/reverse s))
+  (apply str (into '() s)))
+
+
+;; Rich comment block with redefined vars ignored
+#_{:clj-kondo/ignore [:redefined-var]}
+(comment
+
+
+  (defn reverse-string [s]
+    (clojure.string/reverse s))
+
+
+  (defn reverse-string [s]
+    (apply str (reduce conj '() s)))
+
+
+  ) ;; End of rich comment block
