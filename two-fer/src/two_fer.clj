a2af85146dbe05ae0931094b357b1153ce41aa66
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-11-06 18:39:45 +0000

    Two fer: alternative solutions

--- two-fer/src/two_fer.clj
+++ two-fer/src/two_fer.clj
@@ -41,3 +41,37 @@
 
 ;; (two-fer-var-arg-apply-when-let)
 ;; (two-fer-var-arg-apply-when-let "Arthur")
+
+
+;; Reasonable solutions
+
+(ns two-fer)
+
+(defn two-fer
+  ([] (two-fer "you"))
+  ([name] (str "One for " name ", one for me.")))
+
+(defn two-fer [& [name]]
+  (-> (partial format "One for %s, one for me.")
+      (fnil "you")
+      (as-> f (f name))))
+
+;; Common suggestions
+;; Talking points
+
+;; Calling (two-fer "you") vs. returning the string directly.
+
+
+
+(reduce #(str %2 %1) "" "hello")
+
+(reduce conj '() "hello")
+
+
+;; A more idiomatic and preferable approach for production code would be to use the clojure.string/join function.
+
+;; Another approach is to solving this challenge is to use a custom function with reduce.
+
+;; Define a function that creates a string of the two arguments it receives, swapping the order of the arguments. Use that function with reduce and two other arguments, the empty string, "" and the string to be reversed.
+
+;; As reduce iterates over the string to be reversed, it builds up a new string in reverse order.
