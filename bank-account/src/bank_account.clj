b921edfd9f3c28f967c8df1ab7134ab934ae0020
Practicalli Johnny <250870+practicalli-johnny@users.noreply.github.com>
2025-04-04 19:40:35 +0100

    format: wrap repl experiments in comment form
    Signed-off-by: Practicalli Johnny <250870+practicalli-johnny@users.noreply.github.com>

--- bank-account/src/bank_account.clj
+++ bank-account/src/bank_account.clj
@@ -1,6 +1,5 @@
 (ns bank-account)
 
-
 (def bank-accounts
   "All accounts for the bank
   Each account has a unique id and a numeric value"
@@ -14,30 +13,26 @@
     account-id))
 
 (defn close-account [account-id]
-  (swap! bank-accounts dissoc account-id)
-  )
+  (swap! bank-accounts dissoc account-id))
 
 (defn get-balance [account-id]
-  (get @bank-accounts account-id)
-  )
-
-#_(deref bank-accounts)
+  (get @bank-accounts account-id))
 
 (defn update-balance [account-id credit]
   (swap! bank-accounts update account-id #(+ % credit)))
 
-#_(open-account)
+(comment
+  ;; Experimenting with code in the REPL
+  (open-account)
 
-#_(= 0 (-> (open-account)
+  (= 0 (-> (open-account)
            (get-balance)))
 
+  (deref bank-accounts)
+   ;; => {#uuid "af0747fc-86bf-496a-8cc1-efb917887213" 0}
 
-#_(deref bank-accounts)
-;; => {#uuid "af0747fc-86bf-496a-8cc1-efb917887213" 0}
+  (update-balance #uuid "af0747fc-86bf-496a-8cc1-efb917887213" 10)
+  ;; => {#uuid "af0747fc-86bf-496a-8cc1-efb917887213" 10}
 
-#_(update-balance #uuid "af0747fc-86bf-496a-8cc1-efb917887213" 10)
-;; => {#uuid "af0747fc-86bf-496a-8cc1-efb917887213" 10}
-
-
-#_(update-balance #uuid "af0747fc-86bf-496a-8cc1-efb917887213" -10)
-;; => {#uuid "af0747fc-86bf-496a-8cc1-efb917887213" 0}
+  (update-balance #uuid "af0747fc-86bf-496a-8cc1-efb917887213" -10))
+  ;; => {#uuid "af0747fc-86bf-496a-8cc1-efb917887213" 0}
