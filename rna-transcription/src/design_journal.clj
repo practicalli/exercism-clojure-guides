cbd26389a367419c2fdf94d7d90fc7f44fc8793d
Practicalli Engineering <engineering@practical.li>
2025-04-11 16:12:05 +0100

    rna-transcription: deps.edn and journal updates

--- rna-transcription/src/design_journal.clj
+++ rna-transcription/src/design_journal.clj
@@ -1,6 +1,6 @@
 (ns design-journal)
 
-
+;; ---------------------------------------------------------
 ;; Initial thought are to create a hash-map as a simple dictionary lookup.
 ;; Given a DNA nucleotide, e.g a character from the DNA string, find the key that is the same as the character and return the keys value.
 
@@ -11,15 +11,15 @@
 ;; - `T` -> `A`
 ;; - `A` -> `U`
 
-
 (def dictionary-dna-rna
   "Convert DNA to RNA"
   {"G" "C"
    "C" "G"
    "T" "A"
-   "A" "U"}
-  )
+   "A" "U"})
+;; ---------------------------------------------------------
 
+;; ---------------------------------------------------------
 ;; Write a function to get the value for a given key (dna nucleotide)
 
 (defn dna-nucleotide
@@ -44,11 +44,11 @@
     (if (clojure.string/includes? dna "X")
       (throw (AssertionError. "Unknown nucleotide"))
       (clojure.string/join
-        (map #(convert-nucleotide dictionary-dna-rna %) dna))))
+       (map #(convert-nucleotide dictionary-dna-rna %) dna))))
+;; ---------------------------------------------------------
 
-
+;; ---------------------------------------------------------
 ;; Restart in thinking
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; To convert a collection of values, define a hash-map where the keys are the initial value and the values are the transformed value (conversion, encoding, etc).  This is often refered to as a dictionary.
 
@@ -72,11 +72,14 @@
 ;; The result is returned as a sequence of characters.
 ;; Use `clojure.string/join` to return the RNA value as a string
 
+(require '[clojure.string])
+
+#_{:clj-kondo/ignore [:redefined-var]}
 (defn to-rna
   [dna]
   (clojure.string/join
-    (map (fn [nucleotide] (get {\G \C \C \G \T \A \A \U} nucleotide))
-         dna)))
+   (map (fn [nucleotide] (get {\G \C \C \G \T \A \A \U} nucleotide))
+        dna)))
 
 (comment
   (to-rna "GCTA"))
@@ -87,25 +90,29 @@
 
 ;; How about adding the throw as the not-found value?
 
+#_{:clj-kondo/ignore [:redefined-var]}
 (defn to-rna
   [dna]
   (clojure.string/join
-    (map (fn [nucleotide ](get {\G \C \C \G \T \A \A \U} nucleotide
-                               (throw (AssertionError. "Unknown nucleotide")) ))
-         dna)))
+   (map (fn [nucleotide] (get {\G \C \C \G \T \A \A \U} nucleotide
+                              (throw (AssertionError. "Unknown nucleotide"))))
+        dna)))
 
 ;; Unfortunately this will evaluate the throw expression regardless of if the nucleotide is found in the hash-map, so always fails.
+;; ---------------------------------------------------------
 
+;; ---------------------------------------------------------
 ;; `or` function to the rescue
 ;; The `or` function will evaluate the first expression and if a true value is returned then any additional expressions are skipped over.
 ;; If the first expression returns false or a falsey value, i.e. `nil`, then the next expression is evaluated.
 
+#_{:clj-kondo/ignore [:redefined-var]}
 (defn to-rna
   [dna]
   (clojure.string/join
-    (map (fn [nucleotide](or (get {\G \C \C \G \T \A \A \U} nucleotide)
+   (map (fn [nucleotide] (or (get {\G \C \C \G \T \A \A \U} nucleotide)
                              (throw (AssertionError. "Unknown nucleotide"))))
-         dna)))
+        dna)))
 
 (comment
   (to-rna "GCTA")
@@ -114,50 +121,50 @@
   (to-rna "GCXA"))
 
 ;; an AssertionError is thrown as the `X` character does not exist in the dictionary hash-map, so the `get` expression returns `nil`.
+;; ---------------------------------------------------------
 
-
+;; ---------------------------------------------------------
 ;; Refactor and streamline
 
 ;; remove the get
 ;; A hash-map can be called as a function and takes a key as an argument.  This acts the same as the get function, returning the value  associated to a matching key, otherwise returning `nil` or the not-found value if specified.
 
-(defn to-rna
-  [dna]
-  (clojure.string/join
-    (map (fn [nucleotide ](or ({\G \C \C \G \T \A \A \U} nucleotide)
-                              (throw (AssertionError. "Unknown nucleotide"))))
-         dna )))
-
+#_{:clj-kondo/ignore [:redefined-var]}
+(comment
+  (defn to-rna
+    [dna]
+    (clojure.string/join
+     (map (fn [nucleotide] (or ({\G \C \C \G \T \A \A \U} nucleotide)
+                               (throw (AssertionError. "Unknown nucleotide"))))
+          dna))))
 
 ;; using the anonymous function syntax sugar
-`#(* %1 %2)` is the same as `(fn [value1 value2] (+ value1 value2))`
-;; the syntax sugar is often use with `map`, `reduce`, `apply` functions as the function definition tends to be compact and of single use.
+;; `#(* %1 %2) `is the same as `(fn [value1 value2] (+ value1 value2)) `;; the syntax sugar is often use with `map`, `reduce`, `apply` functions as the function definition tends to be compact and of single use.
 
 ;; If the function definition is more complex or used elsewhere in the namespace, then the `defn` function should be used to define shared behavior.
 
-
 (defn to-rna
   [dna]
   (clojure.string/join
-    (map #(or ({\G \C \C \G \T \A \A \U} %)
-              (throw (AssertionError. "Unknown nucleotide")))
-         dna )))
+   (map #(or ({\G \C \C \G \T \A \A \U} %)
+             (throw (AssertionError. "Unknown nucleotide")))
+        dna)))
 
 ;; remove the hard-coded hash-map
 
 (def dictionary-dna-rna {\G \C \C \G \T \A \A \U})
 
+#_{:clj-kondo/ignore [:redefined-var]}
 (defn to-rna
   [dna]
   (clojure.string/join
-    (map #(or (dictionary-dna-rna %)
-              (throw (AssertionError. "Unknown nucleotide")))
-         dna )))
+   (map #(or (dictionary-dna-rna %)
+             (throw (AssertionError. "Unknown nucleotide")))
+        dna)))
+;; ---------------------------------------------------------
 
-
-
-;; Making the function pure
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ---------------------------------------------------------
+;; Pure function approach
 
 ;; Its beyond the scope of the Exercism challenge, however, its recommended to use pure functions where possible.
 ;; A pure function only uses data from its arguments.
@@ -165,20 +172,19 @@
 ;; requires refactoring the unit tests
 ;; Add dna->rna to its own namespace and create parallel tests which use the 2 arity function
 
+#_{:clj-kondo/ignore [:redefined-var]}
 (defn to-rna
   [dictionary dna]
   (clojure.string/join
-    (map #(or (dictionary %)
-              (throw (AssertionError. "Unknown nucleotide")))
-         dna )))
+   (map #(or (dictionary %)
+             (throw (AssertionError. "Unknown nucleotide")))
+        dna)))
 
 ;; With a dictionary as an argument the function is also more usable, as other dictionaries could be used with the function.
 
 ;; The function would now be called as follows
 
-(to-rna dictionary-dna-rna "GTGAC")
-
-
+;; (to-rna dictionary-dna-rna dictionary-dna-rna "GTGAC")
 
 ;; Other interesting points
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -189,7 +195,6 @@
 (into [] "ABCD")
 ;; => [\A \B \C \D]
 
-
 ;; Checks for nucleotides in the dictionary can be done with the `contains?` function.
 ;; This checks to see if the value is a key in the hash-map.
 ;; Contains does not check against the values in a map.  It also checks different things depending on the type of collection.
@@ -197,13 +202,10 @@
 (contains? dictionary-dna-rna "X")
 ;; => false
 
-
 ;; The conversion could take place on the whole rna sequence and check if a correct sequence has been created
 ;; using the map to generate a placeholder value if passed an incorrect nucleotide.
 
 (convert-nucleotide dictionary-dna-rna "B")
-
-
 
 ;; Using Contains to validate the dna sequence
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -225,6 +227,7 @@
 (defn valid-dna? [n]
   (contains? dna->rna n))
 
+#_{:clj-kondo/ignore [:redefined-var]}
 (defn to-rna [dna]
   (assert (every? valid-dna? dna))
   (clojure.string/join (replace dna->rna dna)))
